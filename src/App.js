aeac31064792d52102a5f1482a0833e341dfe57e
Sam Alafar <alafar.hesam@gmail.com>
2021-05-14 13:38:00 +0200

    import Login page and added route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Signup from  './pages/Signup/Signup'
+import Login from './pages/Login/Login'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path='/' component={Signup} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/login' component={Login} />
         </Switch>
       </div>
     )

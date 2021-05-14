c82d9ad367ad43eea12922061c74013a4bef5b04
Bea Avis <bea.avis.p@gmail.com>
2021-05-14 09:30:57 +0200

    Update app.js with router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+// import Signup from './pages/Signup/Signup';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          {/* <Route exact path="/signup" component={Signup} /> */}
+        </Switch>
       </div>
     )
   }

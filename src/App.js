ad77df78807996cbfc811e89f0c5565cd7f8d050
Bea Avis <bea.avis.p@gmail.com>
2021-05-14 19:42:55 +0200

    Update app.js + add app background svg

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-// import Signup from './pages/Signup/Signup';
 import Signup from  './pages/Signup/Signup'
 import Login from './pages/Login/Login'
 

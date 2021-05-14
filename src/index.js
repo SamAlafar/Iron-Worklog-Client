9cc5476a082599c0114e985047d1a184a8eb8bf9
Bea Avis <bea.avis.p@gmail.com>
2021-05-14 09:31:19 +0200

    Added auth provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './context/auth.context';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')

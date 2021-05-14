dc71a64d5f3e57facd8b9836181ae60ce149bd5c
Bea Avis <bea.avis.p@gmail.com>
2021-05-14 14:35:24 +0200

    Add comment with social login button

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react'
 import SignupForm from '../../components/SignupForm/SignupForm'
 import { withAuth } from '../../context/auth.context'
 
-
 class Signup extends Component {
+    handleClick() {
+        this.props.googlelogin();
+    }
+
     render() {
         return (
             <div>
                 <SignupForm signup={this.props.signup}/>
+
+                {/* <button onClick={() => this.handleClick()}>Sign up with google</button> */}
             </div>
         )
     }

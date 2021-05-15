e64eec9661159d34940a146f7d816adafbe28034
Sam Alafar <alafar.hesam@gmail.com>
2021-05-15 22:33:11 +0200

    modified render

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './SignupForm.scss';
 const EMAIL_PATTERN =
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
@@ -60,14 +61,14 @@ export default class SignupForm extends Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.signup(this.state.fields);
-    this.setState({ 
+    this.setState({
       fields: {
         firstName: '',
         lastName: '',
         email: '',
         password: '',
-      }
-    })
+      },
+    });
   }
 
   handleChange(e) {
@@ -87,48 +88,57 @@ export default class SignupForm extends Component {
   render() {
     const { fields } = this.state;
     return (
-      <form onSubmit={(e) => this.handleSubmit(e)}>
-        <div className='form-item'>
-          <label htmlFor='firstName'>First Name: </label>
-          <input
-            type='text'
-            name='firstName'
-            value={fields.firstName}
-            onChange={(e) => this.handleChange(e)}
-          />
-        </div>
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+        <img src='/img/logo.png' alt='logo' />
+          <div className='form-item'>
+            <input
+              htmlFor='firstName'
+              placeholder='First Name'
+              type='text'
+              name='firstName'
+              value={fields.firstName}
+              onChange={(e) => this.handleChange(e)}
+            />
+          </div>
 
-        <div className='form-item'>
-          <label htmlFor='lastName'>Last Name: </label>
-          <input
-            type='text'
-            name='lastName'
-            value={fields.lastName}
-            onChange={(e) => this.handleChange(e)}
-          />
-        </div>
+          <div className='form-item'>
+            <input
+              htmlFor='lastName'
+              placeholder='Last Name'
+              type='text'
+              name='lastName'
+              value={fields.lastName}
+              onChange={(e) => this.handleChange(e)}
+            />
+          </div>
 
-        <div className='form-item'>
-          <label htmlFor='email'>Email: </label>
-          <input
-            type='text'
-            name='email'
-            value={fields.email}
-            onChange={(e) => this.handleChange(e)}
-          />
-        </div>
+          <div className='form-item'>
+            <input
+              htmlFor='email'
+              placeholder='Email'
+              type='text'
+              name='email'
+              value={fields.email}
+              onChange={(e) => this.handleChange(e)}
+            />
+          </div>
 
-        <div className='form-item'>
-          <label htmlFor='password'>Password: </label>
-          <input
-            type='password'
-            name='password'
-            value={fields.password}
-            onChange={(e) => this.handleChange(e)}
-          />
-        </div>
-        <button type='submit'>Sign up</button>
-      </form>
+          <div className='form-item'>
+            <input
+              htmlFor='password'
+              placeholder='Password'
+              type='password'
+              name='password'
+              value={fields.password}
+              onChange={(e) => this.handleChange(e)}
+            />
+          </div>
+          <button className='signup-button' type='submit'>
+            Sign up
+          </button>
+        </form>
+      </div>
     );
   }
 }

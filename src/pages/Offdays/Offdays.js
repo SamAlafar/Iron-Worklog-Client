45668ed89d3447ff9a9bd3e2219c79e699c186af
Bea Avis <bea.avis.p@gmail.com>
2021-05-16 09:53:40 +0200

    Create offdays page

--- src/pages/Offdays/Offdays.js
+++ src/pages/Offdays/Offdays.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import './Offdays.scss';
 import Navbar from '../../components/Navbar/Navbar';
+import AddOffdayForm from '../../components/AddOffdayForm/AddOffdayForm';
 import { withAuth } from '../../context/auth.context';
 
 class Offdays extends Component {
   render() {
     return (
-      <div>
+      <>
         <Navbar />
-        <h1>Offdays</h1>
-      </div>
-    )
+        <div className='page-wrapper'>
+          <h1>Off days</h1>
+          {/* ADD OFFDAY COMPONENT */}
+          <AddOffdayForm />
+          <div>
+            {/* VACATION COMPONENT */}
+            {/* ILLNESS / OFFDAYS COMPONENT */}
+
+          </div>
+        </div>
+      </>
+    );
   }
 }
 

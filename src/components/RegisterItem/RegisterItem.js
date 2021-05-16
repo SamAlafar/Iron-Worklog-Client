ddb0eb3d4b409a0bfe334ba4e5e0d3f57502995e
Sam Alafar <alafar.hesam@gmail.com>
2021-05-16 18:36:55 +0200

    created register item component

---
+++ src/components/RegisterItem/RegisterItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SCRegisterItem from './RegisterItem.styled';
+import { Link } from 'react-router-dom';
+
+export default function RegisterItem() {
+  return (
+    <SCRegisterItem>
+      <div className='registers-wrapper'>
+        <p>Start hour: 09:00</p>
+        <p>End hour: 18:00</p>
+        <p>Start break: 13:30</p>
+        <p>End break: 14:30</p>
+      </div>
+
+      <div className='action-wrapper'>
+        <Link>
+          <i class='fas fa-eye'></i>
+        </Link>
+        <Link>
+          <i class='fas fa-edit'></i>
+        </Link>
+        <Link>
+          <i class='fas fa-trash-alt'></i>
+        </Link>
+      </div>
+    </SCRegisterItem>
+  );
+}
---
+++ src/components/RegisterItem/RegisterItem.styled.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+
+const SCRegisterItem = styled.div`
+  font-size: 2rem;
+  color: #f7fff7;
+  width: 100%;
+  height: 4rem;
+  padding: 2rem;
+  border-radius: 5px;
+  background-color: #1a535c;
+  opacity: 0.75;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .registers-wrapper {
+    display: flex;
+    
+
+    p {
+      padding-right: 2rem;
+    }
+  }
+
+  .action-wrapper {
+    color: #f7fff7;
+
+    a {
+      padding: 0 1rem;
+    }
+
+    .fa-eye {
+      color: #f7fff7;
+    }
+
+    .fa-edit {
+      color: #ffe66d;
+    }
+
+    .fa-trash-alt {
+      color: #ff6b6b;
+    }
+  }
+`;
+
+export default SCRegisterItem;

3c01f51b634c2db6b8b8a21fb68b26609b4b0154
Bea Avis <bea.avis.p@gmail.com>
2021-05-16 13:20:37 +0200

    Update off days styled

--- src/pages/Offdays/Offdays.js
+++ src/pages/Offdays/Offdays.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import SCOffdays from './Offdays.styled';
 import Navbar from '../../components/Navbar/Navbar';
 import AddOffdayForm from '../../components/AddOffdayForm/AddOffdayForm';
+import OffdayItem from '../../components/OffdayItem/OffdayItem';
 import { withAuth } from '../../context/auth.context';
 
 class Offdays extends Component {
+  // STATE WITH OFFDAYS LIST TO SEND BY PROPS TO THE COMPONENTS
+  state = {
+    offdays: [],
+    showAddForm: false,
+  };
+
+  componentDidMount() {
+    // CALL API TO GET ALL OFFDAYS OF THE USER
+    // this.setState({
+    //   offdays: blablabla
+    // })
+  }
+
+  handleShowAdd = () => {
+    this.setState({
+      showAddForm: !this.state.showAddForm,
+    });
+  };
   render() {
     return (
       <>
@@ -12,12 +31,26 @@ class Offdays extends Component {
         <SCOffdays>
           <h1>Off days</h1>
           {/* ADD OFFDAY COMPONENT */}
-          <AddOffdayForm />
-          <div>
-            {/* VACATION COMPONENT */}
-            {/* ILLNESS / OFFDAYS COMPONENT */}
-
+          <div className="add-wrapper">
+            <button className='btn-submit' onClick={() => this.handleShowAdd()}>
+            {!this.state.showAddForm ? 'Add offdays' : 'Close'}
+            </button>
           </div>
+            {this.state.showAddForm ? <AddOffdayForm /> : ''}
+            <div className="lists-wrapper">
+              {/* HOLIDAYS COMPONENT */}
+              <div className="holidays-wrapper">
+                <h2>Holidays</h2>
+                <p>Remaining days: XXX</p>
+              <OffdayItem />
+              </div>
+              {/* ILLNESS / OFFDAYS COMPONENT */}
+              <div className="offdays-wrapper">
+              <h2>Off days and Sick days</h2>
+              <p>Absence days: XXX</p>
+              <OffdayItem />
+              </div>
+            </div>
         </SCOffdays>
       </>
     );
--- src/pages/Offdays/Offdays.styled.js
+++ src/pages/Offdays/Offdays.styled.js
@@ -6,6 +6,32 @@ const SCOffdays = styled.div`
   h1 {
     font-size: 4rem;
   }
+
+  .add-wrapper{
+    text-align: right;
+  }
+
+  .btn-submit{
+    background-color: #FFE66D;
+    color:#1A535C;
+    border-radius: 30px;
+    width: 15rem;
+    height: 3rem;
+    font-size: 1.8rem;
+    filter: drop-shadow(0px 4px 4px rgba(26, 83, 92, 0.7));
+    cursor: pointer;
+  }
+
+  .lists-wrapper {
+    display:flex;
+    justify-content: space-between;
+    text-align: center;
+
+    .holidays-wrapper, .offdays-wrapper{
+      width: 50%;
+      margin: 1.5rem;
+    }
+  }
 `;
 
 export default SCOffdays;

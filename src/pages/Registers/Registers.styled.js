184b864d2f0c66a27d8630ae831606a72b99cec6
Sam Alafar <alafar.hesam@gmail.com>
2021-05-17 16:02:56 +0200

    styled registers page

--- src/pages/Registers/Registers.styled.js
+++ src/pages/Registers/Registers.styled.js
@@ -3,26 +3,31 @@ import styled from 'styled-components'
 const SCRegisters = styled.div`
 padding: 4rem;
 
+.registers-container {
+  display: flex;
+}
   h1 {
     font-size: 4rem;
     padding-bottom: 5rem;
   }
 
   .registers-wrapper {
-      width: 60%;
+      width: 100%;
       margin: 1.5rem;
   }
 
-
-  .calendar {
+  .calendar-wrapper {
     display: flex;
-    align-items: center;
 
     .react-calendar {
-      margin: 1rem 100rem;
+      margin: 1rem 10rem;
       background-color: rgba(247, 255, 247, 0.5);
       border: 2px solid #f7fff7;
     }
+  }
+
+  .register-item{
+    margin-bottom: 10px;
   }
 `;
 

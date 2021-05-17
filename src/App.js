3436710d8597b42ea35e60e998877c1f10a408d1
Sam Alafar <alafar.hesam@gmail.com>
2021-05-17 08:16:38 +0200

    changed name from journeys to registers

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Footer from './components/Footer/Footer'
 import Offdays from './pages/Offdays/Offdays';
-<<<<<<< HEAD
 import Registers from './pages/Registers/Registers'
-=======
 import OffdaysEdit from './pages/OffdaysEdit/OffdaysEdit';
->>>>>>> 6066757fee9908f154bebf69dad54ebcb922d31a
 
 const App = () => {
   return (
@@ -21,11 +18,8 @@ const App = () => {
         <AnonRoute exact path='/signup' component={Signup} />
         <AnonRoute exact path='/login' component={Login} />
         <Route exact path='/offdays' component={Offdays} />
-<<<<<<< HEAD
         <Route exact path='/registers' component={Registers} />
-=======
         <Route exact path='/offdays/edit' component={OffdaysEdit} />
->>>>>>> 6066757fee9908f154bebf69dad54ebcb922d31a
       </Switch>
       <Footer />
     </div>

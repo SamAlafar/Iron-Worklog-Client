9d586e708e53f644ca55a9f1f3279d55304dbe5e
Sam Alafar <alafar.hesam@gmail.com>
2021-05-17 16:01:42 +0200

    deleted handleclick function

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,19 +4,21 @@ import { withAuth } from '../../context/auth.context'
 import {SCButton, SCImage} from './Login.styled'
 
 class Login extends Component {
-    render() {
-        return (
-          <div>
-            <LoginForm login={this.props.login} />
+  handleClick() {
+    this.props.googlelogin();
+  }
+  render() {
+    return (
+      <div>
+        <LoginForm login={this.props.login} />
 
-            <SCButton
-              onClick={() => this.handleClick()}>
-              <SCImage src='/img/google.png' />
-              Login with Google
-            </SCButton>
-          </div>
-        );
-    }
+        <SCButton onClick={() => this.handleClick()}>
+          <SCImage src='/img/google.png' />
+          Login with Google
+        </SCButton>
+      </div>
+    );
+  }
 }
 
 export default withAuth(Login)

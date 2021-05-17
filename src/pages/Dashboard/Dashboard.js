f30fe948c6343e131d23bb15a95a28ba38871344
Bea Avis <bea.avis.p@gmail.com>
2021-05-17 15:42:08 +0200

    Update dashboard structure

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SCDashboard from './Dashboard.styled';
 import Navbar from '../../components/Navbar/Navbar';
 import { withAuth } from '../../context/auth.context';
-import UserInfo from '../../components/UserInfo/UserInfo';
+import OffdayItem from '../../components/OffdayItem/OffdayItem';
+import Calendar from 'react-calendar';
 
-function Dashboard(props) {
-  return (
-    <>
-      <Navbar />
-      <SCDashboard>
-      </SCDashboard>
-    </>
-  );
+class Dashboard extends Component {
+  state = {
+    registers: [],
+    offdays: [],
+  };
+  // INSTANCE OF SERVICES TO CALL AND GET THE INFO
+
+  componentDidMount() {
+    // API CALLS TO GET REGISTERS AND OFFDAYS OF THE USER LOGGED IN
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar />
+        <SCDashboard>
+          <div className="top-section">
+            {/* CREATE REGISTER FORM COMPONENT */}
+            <div className='add-register-container'></div>
+            <Calendar />
+          </div>
+          <div className='lists-wrapper'>
+            <div className='register-container'>
+              {/* MAP REGISTERS OF CURRENT WEEK + DISPLAY REGISTER ITEM IN A LIST */}
+              <OffdayItem />
+            </div>
+            <div className='offdays-container'>
+              {/* MAP OFFDAYS OF CURRENT YEAR + DISPLAY OFFDAYS ITEM IN A LIST */}
+              <OffdayItem />
+            </div>
+          </div>
+        </SCDashboard>
+      </>
+    );
+  }
 }
 
 export default withAuth(Dashboard);

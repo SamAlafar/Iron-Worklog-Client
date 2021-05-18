510470f1e49abd5b5c319f1e264c69141787a596
Sam Alafar <alafar.hesam@gmail.com>
2021-05-18 18:39:05 +0200

    corrected typographic error: instace to instance

--- src/services/journeys.service.js
+++ src/services/journeys.service.js
@@ -10,7 +10,7 @@ export default class JourneyService{
 
     create = data => this.instance.post('/', data);
     get = () => this.instance.get('/');
-    getOne = id => this.instace.get(`/${id}`);
+    getOne = id => this.instance.get(`/${id}`);
     updateOne = (id, data) => this.instance.put(`/${id}`, data);
     deleteOne = id => this.instance.delete(`/${id}`);
 }

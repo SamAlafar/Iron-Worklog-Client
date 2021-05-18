eb1e7b47e5310c472fbbd7013b0869d8664bc39a
Sam Alafar <alafar.hesam@gmail.com>
2021-05-18 21:52:06 +0200

    implemented deleteRegister method

--- src/pages/Registers/Registers.js
+++ src/pages/Registers/Registers.js
@@ -4,6 +4,7 @@ import RegisterItem from '../../components/RegisterItem/RegisterItem';
 import Calendar from 'react-calendar';
 import SCRegisters from './Registers.styled';
 import { withAuth } from '../../context/auth.context';
+import JourneyService from '../../services/journeys.service';
 
 class Registers extends Component {
   //INITIAL STATE OF THE REGISTERS OF EACH USER
@@ -11,11 +12,37 @@ class Registers extends Component {
     registers: [],
   };
 
+  journeyService = new JourneyService();
+
+  deleteRegister(id) {
+    console.log(id)
+    this.journeyService
+    .deleteOne(id)
+    .then((response) => {
+      const registersCopy = 
+      this.state.registers.filter((register) => {
+        console.log(!id === register.id)
+        if(!id === register.id) {
+          return register;
+        }
+      })
+      this.setState({
+        registers: registersCopy
+      })
+    })
+  }
+
   componentDidMount() {
     //CALL API TO GET ALL REGISTERS OF USER AND UPDATE THE INITIAL STATE
-    //this.setState({
-    //    registers: ...
-    // })
+    this.journeyService
+      .get()
+      .then((response) => {
+        this.setState({
+          registers: response.data,
+        });
+        console.log(this.state.registers);
+      })
+      .catch((error) => console.error(error));
   }
 
   render() {
@@ -26,15 +53,9 @@ class Registers extends Component {
           <h1>Registers</h1>
           <div className='registers-container'>
             <div className='registers-wrapper'>
-              <RegisterItem />
-
-              <RegisterItem />
-
-              <RegisterItem />
-
-              <RegisterItem />
-
-              <RegisterItem />
+              {this.state.registers.map((register) => {
+                return <RegisterItem key={register.id} register={register} deleteRegister={() => this.deleteRegister(register.id)} />;
+              })}
             </div>
             <a href='/registers/edit/60a3c21718dee414717111a5'>Link</a>
             <div className='calendar-wrapper'>

0acdcc78c7cbc404d0851f25d8a000f68058b625
Sam Alafar <alafar.hesam@gmail.com>
2021-05-18 18:35:51 +0200

    created class component with functionality

--- src/components/RegisterDetailItem/RegisterDetailItem.js
+++ src/components/RegisterDetailItem/RegisterDetailItem.js
@@ -1,31 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SCRegisterDetailItem from './RegisterDetailItem.styled';
+import dayjs from 'dayjs';
+import JourneyService from '../../services/journeys.service';
+import { withAuth } from '../../context/auth.context'
 
+class RegisterDetailItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {
+        date: dayjs(),
+        startHour: null,
+        endHour: null,
+        startBreak: null,
+        endBreak: null,
+        morningStandup: '',
+        eveningStandup: '',
+      },
+      currentJourneyId: null,
+    };
 
-export default function RegisterDetailItem() {
-  return (
-    <>
-      <SCRegisterDetailItem>
-        <div className='details-wrapper'>
-          <div className='work-hour'>
-            <p className='start-style'>Start Hour: 09:00</p>
-            <p className='end-style'>End Hour: 18:00</p>
-          </div>
-          <div className='break-hour'>
-            <p className='start-style'>Start Break: 13:30</p>
-            <p className='end-style'>End Break: 14:30</p>
-          </div>
-          <div className='worked-hours'>
-            <p className='worked-style'>Worked Hours: 08:30</p>
-            <p className='extra-style'>Extra Hours: 00:30</p>
-          </div>
-        </div>
-        <div className='standup-wrapper'>
-          <p>Prework Standup</p>
-          <p>Afterwork Standup</p>
-        </div>
+    this.journeyService = new JourneyService();
+  }
+  componentDidMount() {
+    console.log(this.props)
+    this.journeyService.getOne(this.props.match.params.id).then((response) => {
+      this.setState({
+        fields: response.data,
+      });
+    });
+  }
 
-      </SCRegisterDetailItem>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <SCRegisterDetailItem>
+          <div className='details-wrapper'>
+            <div className='work-hour'>
+              <p className='start-style'>
+                Start Hour: {this.state.fields.startHour}
+              </p>
+              <p className='end-style'>End Hour: {this.state.fields.endHour}</p>
+            </div>
+            <div className='break-hour'>
+              <p className='start-style'>
+                Start Break: {this.state.fields.startBreak}
+              </p>
+              <p className='end-style'>
+                End Break: {this.state.fields.endBreak}
+              </p>
+            </div>
+            <div className='worked-hours'>
+              <p className='worked-style'>Worked Hours: 08:30</p>
+              <p className='extra-style'>Extra Hours: 00:30</p>
+            </div>
+          </div>
+          <div className='standup-wrapper'>
+            <p>Prework Standup</p>
+            <p>Afterwork Standup</p>
+          </div>
+        </SCRegisterDetailItem>
+      </>
+    );
+  }
 }
+
+export default withAuth(RegisterDetailItem);

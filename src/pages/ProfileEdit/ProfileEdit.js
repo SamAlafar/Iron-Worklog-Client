67277e0cf0d77244063819c49372b51cb5a1809c
Sam Alafar <alafar.hesam@gmail.com>
2021-05-20 01:20:57 +0200

    imported EditProfileForm component and withAuth

--- src/pages/ProfileEdit/ProfileEdit.js
+++ src/pages/ProfileEdit/ProfileEdit.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import EditProfileForm from '../../components/EditProfileForm/EditProfileForm';
+import { withAuth } from '../../context/auth.context' 
 
-export default function ProfileEdit() {
+ function ProfileEdit(props) {
   return (
     <div>
       <Navbar />
+      <EditProfileForm user={props.user}/>
     </div>
   )
 }
+
+export default withAuth(ProfileEdit);

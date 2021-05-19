61fd5c686002cbb2207e1b60284bc407d2a3154e
Bea Avis <bea.avis.p@gmail.com>
2021-05-19 19:34:36 +0200

    Updated offdays edit route

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
         <PrivateRoute exact path='/profile' component={Profile} />
         <PrivateRoute exact path='/profile-edit' component={ProfileEdit} />
         <PrivateRoute exact path='/offdays' component={Offdays} />
-        <PrivateRoute exact path='/offdays/edit' component={OffdaysEdit} />
+        <PrivateRoute exact path='/offdays/edit/:id' component={OffdaysEdit} />
         <PrivateRoute exact path='/registers/:id' component={RegisterDetails} />
         <PrivateRoute exact path='/registers/edit/:id' component={RegisterEdit} />
       </Switch>

96443f67813dfbadee1342c2d72b60c662ad631a
Bea Avis <bea.avis.p@gmail.com>
2021-05-19 19:37:43 +0200

    Connect offday item to api to read data

--- src/components/OffdayItem/OffdayItem.js
+++ src/components/OffdayItem/OffdayItem.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import SCOffdayItem from './OffdayItem.styled';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 
-export default function OffdayItem() {
+export default function OffdayItem({ offday, deleteOffday }) {
   return (
-    <SCOffdayItem className="offday-item">
-      <div className="days-wrapper">
-
-        <p>Start day: 25/05/2021</p>
-        <p>End day: 30/05/2021</p>
+    <SCOffdayItem className='offday-item'>
+      <div className='days-wrapper'>
+        <span>{}</span>
+        <p>Start day: {dayjs(offday.startDay).format('DD/MM/YYYY')}</p>
+        <p>End day:{dayjs(offday.endDay).format('DD/MM/YYYY')}</p>
       </div>
-        <div className="action-wrapper">
-          <Link to="/offdays/edit"><i className="fas fa-edit"></i></Link>
-          <Link to=""><i className="fas fa-trash-alt"></i></Link>
-        </div>
+      <div className='action-wrapper'>
+        <Link
+          to={`/offdays/edit/${offday.id}`}>
+          <i className='fas fa-edit'></i>
+        </Link>
+        <Link onClick={deleteOffday}>
+          <i className='fas fa-trash-alt'></i>
+        </Link>
+      </div>
     </SCOffdayItem>
-  )
+  );
 }

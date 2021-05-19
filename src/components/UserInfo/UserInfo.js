bfbad803221df714eff29cb6d751da93abcd46f5
Sam Alafar <alafar.hesam@gmail.com>
2021-05-20 01:19:09 +0200

    imported dayjs, modified return structure

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import SCUserInfo from './UserInfo.styled';
+import dayjs from 'dayjs';
 
 export default function UserInfo({ user }) {
   return (
     <SCUserInfo className='user-info-wrapper'>
       <div className='user-info'>
-        <div>
-          <p>Employee name: {user.firstName}</p>
-          <p>Employee surname: {user.lastName}</p>
+        <div className='name-wrapper'>
+          <label>
+            Employee Name
+            <p>{user.firstName}</p>
+          </label>
+          <label>
+            Employee Surname
+            <p>{user.lastName}</p>
+          </label>
         </div>
-        <div>
-          <p>Employee birthdate: {user.birthDate}</p>
-          <p>Contract start date: {user.startContract}</p>
+        <div className='details-wrapper'>
+          <label>
+            Employee Birth Date
+            <p>{dayjs(user.birthDate).format('DD/MM/YYYY')}</p>
+          </label>
+          <label>
+            Contract Start Date
+            <p>
+              {dayjs(user.startContract).format('DD/MM/YYYY')}
+            </p>
+          </label>
         </div>
       </div>
-      <img src={user.profilePic} alt='User photo' />
+      <div className='profile-pic'>
+        <img src={user.profilePic} alt='User photo' />
+      </div>
     </SCUserInfo>
   );
 }

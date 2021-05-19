3d0184d9f3830bb7f167fcfbce17bb56ff193373
Bea Avis <bea.avis.p@gmail.com>
2021-05-19 19:35:37 +0200

    Style dashboard lists

--- src/pages/Dashboard/Dashboard.styled.js
+++ src/pages/Dashboard/Dashboard.styled.js
@@ -9,6 +9,30 @@ const SCDashboard = styled.div`
   .lists-wrapper {
     display: flex;
     justify-content: space-evenly;
+    text-align: center;
+
+    h2 {
+      font-size: 1.8rem;
+      padding: 5px;
+    }
+
+    .offdays-counter, .registers-counter{
+      font-size: 1.5rem;
+      padding: 5px;
+    }
+
+    .registers-wrapper, .offdays-wrapper{
+      margin: 1.5rem;
+
+      .list-header{
+        background-color: #F7FFF7;
+        border-radius: 5px;
+      }
+
+      .offday-item, .register-item{
+        margin-bottom: 10px;
+      }
+    }
   }
 `;
 

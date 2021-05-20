121ab1571eb62456eebc6fd0ba038ef6ef8e7cfb
Sam Alafar <alafar.hesam@gmail.com>
2021-05-20 11:46:57 +0200

    modified styling register edit item

--- src/components/RegisterEditItem/RegisterEditItem.styled.js
+++ src/components/RegisterEditItem/RegisterEditItem.styled.js
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 const SCRegisterEditItem = styled.form`
   font-size: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   .details-wrapper {
     margin-top: 3rem;
     display: flex;
@@ -10,28 +13,42 @@ const SCRegisterEditItem = styled.form`
     text-align: center;
   }
 
+  .time-picker {
+    width: 10px;
+    height: 35px;
+    padding-bottom: 10px;
+  }
+
+  .time-picker-end {
+    width: 10px;
+    height: 35px;
+    padding-bottom: 10px;
+  }
   .work-hour > p {
     margin-bottom: 30px;
+    padding-left: 20px;
     padding-top: 15px;
-
+    display: flex;
   }
 
   .break-hour > p {
     margin-bottom: 30px;
     margin-left: 40px;
+    padding-left: 20px;
     padding-top: 15px;
+    display: flex;
   }
 
   .start-style {
     background-color: #ffe66d;
-    width: 190px;
+    width: 270px;
     height: 50px;
     border-radius: 30px;
   }
 
   .end-style {
     background-color: #ff6b6b;
-    width: 190px;
+    width: 270px;
     height: 50px;
     border-radius: 30px;
   }
@@ -40,26 +57,25 @@ const SCRegisterEditItem = styled.form`
     display: flex;
     align-items: center;
     justify-content: center;
-    padding-top: 30px;
-  }
-
-  .standup-wrapper > textarea {
-    text-align: left;
-    margin-right: 20px;
-    padding-top: 10px;
-    border: 2px solid #f7fff7;
-    width: 350px;
-    height: 350px;
-    background-color: rgba(247, 255, 247, 0.4);
-    border-radius: 5px;
-  }
-
-  .standup-label {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    text-decoration: underline;
-    margin-top: 20px;
+    padding-top: 50px;
+    label {
+      display: flex;
+      flex-direction: column;
+      text-decoration: underline;
+      text-align: center;
+      padding-bottom: 10px;
+    }
+    textarea {
+      text-align: left;
+      margin-right: 20px;
+      padding-top: 10px;
+      border: 2px solid #f7fff7;
+      width: 350px;
+      height: 150px;
+      background-color: rgba(247, 255, 247, 0.4);
+      border-radius: 5px;
+      margin-bottom: 5px;
+    }
   }
 
   .btn-submit {
@@ -69,13 +85,8 @@ const SCRegisterEditItem = styled.form`
     height: 38px;
     border-radius: 10px;
     color: #ffe66d;
-    display: flex;
-    align-ietms: center;
-    justify-content: center;
-    margin-left: 830px;
     margin-top: 30px;
   }
-
 `;
 
 export default SCRegisterEditItem;
